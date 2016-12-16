f5d96d193fc57a3fc45e025ee6a0d9bfcbba2397
Young Lee Student - datasciencecoursera <kcoala@yahoo.com>
2016-12-16 09:08:22 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,2 @@
-### Merges the training and the test sets to create one data set.
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement
