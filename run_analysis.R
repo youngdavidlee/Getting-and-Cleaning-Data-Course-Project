5c36d789c6edd23ba63b38883749047316e58da0
Young Lee Student - datasciencecoursera <kcoala@yahoo.com>
2017-01-23 18:53:57 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,3 @@
-# 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement
-# 3. Uses descriptive activity names to name the activities in the data set.
-# 4. Appropriately labels the data set with descriptive variable names.
-# 5. From the data set in step 4, creates a second independent tidy set with the average of each variable for each activity and each subject.
 setwd("/Users/davidlee/UCI HAR Dataset")
 
 #activity_labels.txt   [6 different types of activities]
@@ -12,6 +7,7 @@ setwd("/Users/davidlee/UCI HAR Dataset")
 #4 SITTING
 #5 STANDING
 #6 LAYING 
+
 activities_6 <- read.csv("activity_labels.txt", sep = "", header = FALSE)
 
 ##features.txt   [561 feature vectors]
@@ -27,7 +23,7 @@ testMoves <- read.csv("test/Y_test.txt", sep = "", header = FALSE)
 trainMoves <- read.csv("train/Y_train.txt", sep = "", header = FALSE)
 combinedMoves <- rbind(testMoves, trainMoves)
 
-#Load PersonID [30 Person total; 9 Person in testPerson; 21 Person in trainPerson]
+#Load Person [30 Person total; 9 Person in testPerson; 21 Person in trainPerson]
 testPerson <- read.csv("test/subject_test.txt", sep = "", header = FALSE)
 trainPerson <- read.csv("train/subject_train.txt", sep = "", header = FALSE)
 combinedPerson <- rbind(testPerson, trainPerson)
